9525da799eb6ec55f33f99e6d41292bfd0457ee4
aeon <jerry73204@gmail.com>
2022-12-02 05:55:33 +0800

    Change a test to an example

---
+++ carla/examples/simple.rs
@@ -0,0 +1,61 @@
+use carla::client::{Client, TrySpawnActorOptions};
+use nalgebra::{Isometry3, Translation3, UnitQuaternion};
+use std::time::{Duration, Instant};
+
+fn main() {
+    let mut client = Client::default();
+    client.set_timeout(Duration::from_secs(5));
+    let mut world = client.load_world("Town07");
+
+    let spectator = world.spectator();
+    // let spectator = spectator
+    //     .try_into_sensor()
+    //     .unwrap_or_else(|_| panic!("spectator is not a sensor"));
+    // spectator.listen(move |_data| {
+    //     println!("data received");
+    // });
+
+    let blulib = world.blueprint_library();
+    // blulib.iter().for_each(|blu| {
+    //     println!("{}", blu.id());
+    // });
+
+    let vehicle = {
+        let blu = blulib.find("vehicle.tesla.model3").unwrap();
+        let pose = Isometry3 {
+            translation: Translation3::new(72.31, -7.55, 1.0),
+            rotation: UnitQuaternion::from_euler_angles(0.01, 1.13, -62.79),
+        };
+        let actor = world.try_spawn_actor(&blu, &pose).unwrap();
+        actor
+            .try_into_vehicle()
+            .unwrap_or_else(|_| panic!("not a vehicle"))
+    };
+
+    let lidar = {
+        let blu = blulib.find("sensor.camera.rgb").unwrap();
+        let actor = world
+            .try_spawn_actor_opt(
+                &blu,
+                &Isometry3::identity(),
+                TrySpawnActorOptions {
+                    parent: Some(&vehicle),
+                    ..Default::default()
+                },
+            )
+            .unwrap();
+        actor
+            .try_into_sensor()
+            .unwrap_or_else(|_| panic!("not a sensor"))
+    };
+
+    // lidar.listen(move |_data| {
+    //     println!("data received");
+    // });
+
+    loop {
+        let since = Instant::now();
+        world.wait_for_tick();
+        dbg!(since.elapsed());
+    }
+}
