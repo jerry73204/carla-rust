931598cbc07d38c539c34aa44c329859ce64f6b3
aeon <jerry73204@gmail.com>
2022-12-06 11:07:19 +0800

    Add prelude mod

--- carla/src/lib.rs
+++ carla/src/lib.rs
@@ -3,3 +3,14 @@ pub mod geom;
 pub mod road;
 pub mod rpc;
 pub mod sensor;
+
+pub mod prelude {
+    pub use crate::{
+        client::ActorBase as _,
+        geom::{
+            LocationExt as _, RotationExt as _, TransformExt as _, Vector2DExt as _,
+            Vector3DExt as _,
+        },
+        sensor::SensorData as _,
+    };
+}
