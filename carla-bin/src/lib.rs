c8b3aa3ca11587c7ff31ba544960f25c8df6d812
aeon <jerry73204@gmail.com>
2022-12-11 00:32:50 +0800

    Download prebuilt binaries from GitHub release link

--- carla-bin/src/lib.rs
+++ carla-bin/src/lib.rs
@@ -1,10 +1,13 @@
-use anyhow::{Context, Result};
+use anyhow::{ensure, Context, Result};
 use carla_src::{libcarla_client, probe};
 use once_cell::sync::OnceCell;
 use std::{
     fs::{self, OpenOptions},
+    io::{self, BufReader},
     path::{Path, PathBuf},
 };
+use tar::Archive;
+use xz::bufread::XzDecoder;
 
 pub struct CarlaBuild {
     pub include_dirs: Vec<PathBuf>,
@@ -13,6 +16,11 @@ pub struct CarlaBuild {
 
 const PREBUILD_DIR_NAME: &str =
     include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/PREBUILD_DIR_NAME"));
+const DOWNLOAD_URL: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/DOWNLOAD_URL"));
+const OUT_DIR: &str = env!("OUT_DIR");
+const TAG: &str = include_str!(concat!(env!("OUT_DIR"), "/TAG"));
+const BUILD_READY_FILE: &str = concat!(env!("OUT_DIR"), "/BUILD_READY");
+const DOWNLOAD_READY_FILE: &str = concat!(env!("OUT_DIR"), "/DOWNLOAD_READY");
 
 /// Prepares the Carla simulator source code and build client library
 /// when called.
@@ -23,25 +31,10 @@ pub fn build_carla() -> Result<&'static CarlaBuild> {
 }
 
 fn cached_or_build() -> Result<CarlaBuild> {
-    let out_dir = Path::new(env!("OUT_DIR"));
-    let manifest_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
-    let build_ready_file = out_dir.join("carla_build_ready");
-    let tag_file = out_dir.join("TAG");
-    let tag = fs::read_to_string(&tag_file)?;
-    let prebuild_dir = manifest_dir.join(PREBUILD_DIR_NAME).join(&tag);
-
     // Try to use cached files
     #[cfg(not(feature = "force-rebuild"))]
-    {
-        if prebuild_dir.is_dir() {
-            let include_dir = prebuild_dir.join("include");
-            let lib_dir = prebuild_dir.join("lib");
-
-            return Ok(CarlaBuild {
-                include_dirs: vec![include_dir],
-                lib_dirs: vec![lib_dir],
-            });
-        }
+    if let Some(build) = load_cache()? {
+        return Ok(build);
     }
 
     // Download source code
@@ -51,7 +44,8 @@ fn cached_or_build() -> Result<CarlaBuild> {
     let probe = probe(&src_dir);
 
     // Build if it wasn't done before
-    skip_or_run(&build_ready_file, || {
+    let build_ready_file = Path::new(BUILD_READY_FILE);
+    skip_or_run(build_ready_file, || {
         libcarla_client::build(&src_dir)?;
         anyhow::Ok(())
     })?;
@@ -60,6 +54,62 @@ fn cached_or_build() -> Result<CarlaBuild> {
         include_dirs: probe.include_dirs.into_vec(),
         lib_dirs: probe.lib_dirs.into_vec(),
     })
+}
+
+fn load_cache() -> Result<Option<CarlaBuild>> {
+    cached_or_download()?;
+    let install_dir = install_dir();
+
+    if !install_dir.is_dir() {
+        return Ok(None);
+    }
+
+    let include_dir = install_dir.join("include");
+    let lib_dir = install_dir.join("lib");
+
+    Ok(Some(CarlaBuild {
+        include_dirs: vec![include_dir],
+        lib_dirs: vec![lib_dir],
+    }))
+}
+
+fn cached_or_download() -> Result<()> {
+    let download_ready_file = Path::new(DOWNLOAD_READY_FILE);
+    skip_or_run(download_ready_file, download_prebuild)?;
+    Ok(())
+}
+
+fn download_prebuild() -> Result<()> {
+    let prebuild_dir = prebuild_dir();
+
+    // Clean the dir to extract to
+    let result = fs::remove_dir_all(&prebuild_dir);
+    match result {
+        Ok(()) => {}
+        Err(err) if err.kind() == io::ErrorKind::NotFound => {}
+        Err(err) => return Err(err.into()),
+    }
+
+    // Download and extract to prebuild_dir
+    let reader = ureq::get(DOWNLOAD_URL)
+        .call()
+        .with_context(|| format!("Failed to download from URL '{}'", DOWNLOAD_URL))?
+        .into_reader();
+    let reader = BufReader::new(reader);
+    let mut archive = Archive::new(XzDecoder::new(reader));
+    archive
+        .unpack(OUT_DIR)
+        .with_context(|| format!("Failed to unpack file downloaded from '{}'", DOWNLOAD_URL))?;
+
+    // Check if desired extracted files exist
+    ensure!(
+        prebuild_dir.is_dir(),
+        "'{}' does not exist. Is the URL '{}' correct?",
+        prebuild_dir.display(),
+        DOWNLOAD_URL
+    );
+
+    Ok(())
 }
 
 fn skip_or_run<T, F>(target_path: &Path, callback: F) -> Result<Option<T>>
@@ -77,4 +127,12 @@ where
 fn touch(path: &Path) -> Result<()> {
     OpenOptions::new().create(true).write(true).open(path)?;
     Ok(())
+}
+
+fn prebuild_dir() -> PathBuf {
+    Path::new(OUT_DIR).join(PREBUILD_DIR_NAME)
+}
+
+fn install_dir() -> PathBuf {
+    prebuild_dir().join(TAG)
 }
