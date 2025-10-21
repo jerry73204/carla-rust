25a8cc9f19ac4c448de35c63548af035efbc2f59
aeon <jerry73204@gmail.com>
2025-10-21 17:30:02 +0800

    Rename module names to avoid name conflict.
    - client.rs => carla_client.rs
    - traffic_manager.rs => tm.rs

--- carla/src/traffic_manager.rs
+++ carla/src/traffic_manager.rs
@@ -3,9 +3,9 @@
 
 mod action;
 mod action_buffer;
-mod traffic_manager;
+mod tm;
 
 pub use action::*;
 pub use action_buffer::*;
 pub use carla_sys::carla::traffic_manager::{constants, RoadOption};
-pub use traffic_manager::*;
+pub use tm::*;
