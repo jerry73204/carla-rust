f1f3c3605611dc137635113826718298aca4924a
aeon <jerry73204@gmail.com>
2025-10-29 04:54:08 +0800

    Implement collision and lane invasion event

--- carla/src/sensor/data/collision_event.rs
+++ carla/src/sensor/data/collision_event.rs
@@ -1,12 +1,65 @@
 // SAFETY: This module uses unwrap_unchecked() for performance on methods guaranteed
 // to never return null. See UNWRAP_REPLACEMENTS.md for detailed C++ code audit.
 
+//! Collision event sensor data.
+//!
+//! This module provides the [`CollisionEvent`] type for detecting and measuring collisions
+//! between actors in the simulation.
+
 use crate::{client::Actor, sensor::SensorData};
 use carla_sys::{carla::geom::Vector3D, carla_rust::sensor::data::FfiCollisionEvent};
 use cxx::SharedPtr;
 use derivative::Derivative;
 use static_assertions::assert_impl_all;
 
+/// Collision event data from a collision sensor.
+///
+/// Generated when an actor with a collision sensor attached collides with another actor.
+/// Contains information about the collision force and the actors involved.
+///
+/// Corresponds to [`carla.CollisionEvent`](https://carla.readthedocs.io/en/0.9.14/python_api/#carla.CollisionEvent) in the Python API.
+///
+/// # Examples
+///
+/// ```no_run
+/// use carla::{
+///     client::Client,
+///     sensor::{data::CollisionEvent, SensorDataBase},
+/// };
+///
+/// let client = Client::default();
+/// let mut world = client.world();
+///
+/// // Spawn vehicle with collision sensor
+/// let bp_lib = world.blueprint_library();
+/// let vehicle_bp = bp_lib.filter("vehicle.*").get(0).unwrap();
+/// let collision_bp = bp_lib.find("sensor.other.collision").unwrap();
+///
+/// let spawn_points = world.map().recommended_spawn_points();
+/// let vehicle = world
+///     .spawn_actor(&vehicle_bp, &spawn_points.get(0).unwrap())
+///     .unwrap();
+///
+/// let collision_sensor = world
+///     .spawn_actor_opt(
+///         &collision_bp,
+///         &nalgebra::Isometry3::identity(),
+///         Some(&vehicle),
+///         carla::rpc::AttachmentType::Rigid,
+///     )
+///     .unwrap();
+///
+/// let sensor: carla::client::Sensor = collision_sensor.try_into().unwrap();
+/// sensor.listen(move |data| {
+///     if let Ok(collision) = CollisionEvent::try_from(data) {
+///         println!("Collision detected!");
+///         println!("  Impulse: {:?}", collision.normal_impulse());
+///         if let Some(other) = collision.other_actor() {
+///             println!("  Hit actor ID: {}", other.id());
+///         }
+///     }
+/// });
+/// ```
 #[derive(Clone, Derivative)]
 #[derivative(Debug)]
 #[repr(transparent)]
@@ -16,14 +69,57 @@ pub struct CollisionEvent {
 }
 
 impl CollisionEvent {
+    /// Returns the actor that owns the collision sensor.
+    ///
+    /// This is the actor that experienced the collision and has the sensor attached.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// # use carla::sensor::data::CollisionEvent;
+    /// # let collision: CollisionEvent = todo!();
+    /// let actor = collision.actor();
+    /// println!("Collision sensor actor ID: {}", actor.id());
+    /// ```
     pub fn actor(&self) -> Actor {
         unsafe { Actor::from_cxx(self.inner.GetActor()).unwrap_unchecked() }
     }
 
+    /// Returns the actor that was hit in the collision, if any.
+    ///
+    /// Returns `None` if the collision was with a static object (like a building or the ground).
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// # use carla::sensor::data::CollisionEvent;
+    /// # let collision: CollisionEvent = todo!();
+    /// if let Some(other) = collision.other_actor() {
+    ///     println!("Collided with actor ID: {}", other.id());
+    /// } else {
+    ///     println!("Collided with static object");
+    /// }
+    /// ```
     pub fn other_actor(&self) -> Option<Actor> {
         Actor::from_cxx(self.inner.GetOtherActor())
     }
 
+    /// Returns the normal impulse resulting from the collision.
+    ///
+    /// The impulse is a 3D vector representing the force applied during the collision.
+    /// The magnitude indicates collision severity, and the direction shows the impact vector.
+    ///
+    /// Units: Newton-seconds (N·s)
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// # use carla::sensor::data::CollisionEvent;
+    /// # let collision: CollisionEvent = todo!();
+    /// let impulse = collision.normal_impulse();
+    /// let magnitude = (impulse.x * impulse.x + impulse.y * impulse.y + impulse.z * impulse.z).sqrt();
+    /// println!("Collision force: {} N·s", magnitude);
+    /// ```
     pub fn normal_impulse(&self) -> &Vector3D {
         self.inner.GetNormalImpulse()
     }
--- carla/src/sensor/data/lane_invasion_event.rs
+++ carla/src/sensor/data/lane_invasion_event.rs
@@ -1,6 +1,11 @@
 // SAFETY: This module uses unwrap_unchecked() for performance on methods guaranteed
 // to never return null. See UNWRAP_REPLACEMENTS.md for detailed C++ code audit.
 
+//! Lane invasion event sensor data.
+//!
+//! This module provides the [`LaneInvasionEvent`] type for detecting when an actor
+//! crosses lane markings.
+
 use crate::{client::Actor, road::element::LaneMarking, sensor::SensorData};
 use autocxx::prelude::*;
 use carla_sys::carla_rust::sensor::data::FfiLaneInvasionEvent;
@@ -8,6 +13,59 @@ use cxx::SharedPtr;
 use derivative::Derivative;
 use static_assertions::assert_impl_all;
 
+/// Lane invasion event data from a lane invasion sensor.
+///
+/// Generated when an actor with a lane invasion sensor crosses one or more lane markings.
+/// Useful for implementing lane departure warnings and autonomous driving assistance.
+///
+/// Corresponds to [`carla.LaneInvasionEvent`](https://carla.readthedocs.io/en/0.9.14/python_api/#carla.LaneInvasionEvent) in the Python API.
+///
+/// # Examples
+///
+/// ```no_run
+/// use carla::{
+///     client::Client,
+///     sensor::{data::LaneInvasionEvent, SensorDataBase},
+/// };
+///
+/// let client = Client::default();
+/// let mut world = client.world();
+///
+/// // Spawn vehicle with lane invasion sensor
+/// let bp_lib = world.blueprint_library();
+/// let vehicle_bp = bp_lib.filter("vehicle.*").get(0).unwrap();
+/// let lane_invasion_bp = bp_lib.find("sensor.other.lane_invasion").unwrap();
+///
+/// let spawn_points = world.map().recommended_spawn_points();
+/// let vehicle = world
+///     .spawn_actor(&vehicle_bp, &spawn_points.get(0).unwrap())
+///     .unwrap();
+///
+/// let lane_sensor = world
+///     .spawn_actor_opt(
+///         &lane_invasion_bp,
+///         &nalgebra::Isometry3::identity(),
+///         Some(&vehicle),
+///         carla::rpc::AttachmentType::Rigid,
+///     )
+///     .unwrap();
+///
+/// let sensor: carla::client::Sensor = lane_sensor.try_into().unwrap();
+/// sensor.listen(move |data| {
+///     if let Ok(lane_invasion) = LaneInvasionEvent::try_from(data) {
+///         println!("Lane departure detected!");
+///         let markings = lane_invasion.crossed_lane_markings();
+///         println!("  Crossed {} lane marking(s)", markings.len());
+///         for marking in markings {
+///             println!(
+///                 "    Type: {:?}, Color: {:?}",
+///                 marking.lane_type(),
+///                 marking.color()
+///             );
+///         }
+///     }
+/// });
+/// ```
 #[derive(Clone, Derivative)]
 #[derivative(Debug)]
 #[repr(transparent)]
@@ -17,10 +75,40 @@ pub struct LaneInvasionEvent {
 }
 
 impl LaneInvasionEvent {
+    /// Returns the actor that owns the lane invasion sensor.
+    ///
+    /// This is the actor that crossed the lane markings and has the sensor attached.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// # use carla::sensor::data::LaneInvasionEvent;
+    /// # let lane_invasion: LaneInvasionEvent = todo!();
+    /// let actor = lane_invasion.actor();
+    /// println!("Lane invasion actor ID: {}", actor.id());
+    /// ```
     pub fn actor(&self) -> Actor {
         unsafe { Actor::from_cxx(self.inner.GetActor()).unwrap_unchecked() }
     }
 
+    /// Returns the list of lane markings that were crossed.
+    ///
+    /// Each [`LaneMarking`] contains information about the type (solid, broken, etc.),
+    /// color (white, yellow, etc.), and width of the marking.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// # use carla::sensor::data::LaneInvasionEvent;
+    /// # let lane_invasion: LaneInvasionEvent = todo!();
+    /// let markings = lane_invasion.crossed_lane_markings();
+    /// println!("Crossed {} lane marking(s)", markings.len());
+    /// for marking in markings {
+    ///     println!("  Type: {:?}", marking.lane_type());
+    ///     println!("  Color: {:?}", marking.color());
+    ///     println!("  Width: {}", marking.width());
+    /// }
+    /// ```
     pub fn crossed_lane_markings(&self) -> Vec<LaneMarking> {
         self.inner
             .GetCrossedLaneMarkings()
